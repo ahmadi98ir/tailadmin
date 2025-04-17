c6a764bc70112a6cc5227d46b0007490d8e046dd
ahmadi98 <info@ahmadi98.ir>
2025-04-17 23:26:39 +0100

    اضافه کردن تنظیمات جدید به next.config.js شامل دامنه‌های مجاز برای تصاویر و بازنویسی درخواست‌ها به API. همچنین، اصلاح اسکریپت شروع در package.json برای پشتیبانی از پورت پیش‌فرض 3000 و پورت لینوکس.

--- next.config.js
+++ next.config.js
@@ -12,11 +12,21 @@ const nextConfig = {
   trailingSlash: true, // این باعث می‌شود که URL‌ها با / پایان یابند
   images: {
     unoptimized: true, // حل مشکل بهینه‌سازی تصاویر
+    domains: ['localhost'],
   },
   poweredByHeader: false,
   reactStrictMode: false,
   // حل مشکل دسترسی به فایل‌های استاتیک
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  // اضافه کردن تنظیمات باز نویسی درخواست‌ها
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api'}/:path*`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig; 
